6121db86a26efaa29371b1d8c4e2a149a0f8913e
kavyahallur123 <kavyahallur199@gmail.com>
2022-12-25 19:28:53 +0530

    Added: Welcome to Address Book System

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.addressbook;
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Wel come to adderess book system using streams");
+    }
+}
